8d8e92e298f6c38d44aaf0c56171f386c399ccd8
Samantha Brown <bro7445@gmail.com>
2021-05-16 20:58:55 -0400

    Restaurant push

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,11 +1,25 @@
 
-function createRestaurant() {
-  
+function createRestaurant(name) {
+    return {
+      name: name,
+      menus: {
+        breakfast: [],
+        lunch: [],
+        dinner:[]
+   }
+ }
 }
+
+function addMenuItem(restaurant, item) {
+if (!restaurant.menus[item.type].includes(item)) {
+restaurant.menus[item.type].push(item);
+}
+  }
+
 
 
 module.exports = {
   createRestaurant,
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem
 }
